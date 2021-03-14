3d65fce8cb3ef273aeb4407cb45904b7c023ed76
melkia bustamante <bonfil99melkia@gmail.com>
2021-03-13 22:29:48 -0600

    menu hamburguesa añadido4

--- src/app/auth/components/header/header.component.ts
+++ src/app/auth/components/header/header.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  menuActive = false;
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+  onClickMenu(){
+    this.menuActive = !this.menuActive;
+    let navbarMenu = document.querySelector('.navbar-menu');
+    if(this.menuActive){
+      navbarMenu.classList.add('show');
+      navbarMenu.classList.remove('hidden');
+    }else{      
+      navbarMenu.classList.add('hidden');
+      navbarMenu.classList.remove('show');
+    }
   }
 
+  closeMenu(){
+    let navbarMenu = document.querySelector('.navbar-menu');
+    navbarMenu.classList.add('hidden');
+    navbarMenu.classList.remove('show');
+    this.menuActive = false;
+  }
 }

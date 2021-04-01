36478b77681a60a8b8de66f9767d80c20a3a024f
melkia bustamante <bonfil99melkia@gmail.com>
2021-03-31 18:15:43 -0600

    Form added

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
 // Routing module
+import { AuthRoutingModule } from './auth-routing.module';
+
 //Components
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterAdminComponent } from './pages/register-admin/register-admin.component';
@@ -9,13 +13,13 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LayoutComponent } from './layout/layout/layout.component';
 
-import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
   declarations: [LoginComponent, RegisterAdminComponent, RegisterPatientComponent, HeaderComponent, FooterComponent, LayoutComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule
   ],
   exports: []
 })
--- src/app/auth/pages/register-patient/register-patient.component.ts
+++ src/app/auth/pages/register-patient/register-patient.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+
+// Added by @Melkia
+// Regular expression to deal with names. Only letters and spaces are allowed.
+// TODO: For scalability put all regex in a file 
+const REGEX_NAME = /^[a-zA-Z ]{3,30}$/;
+
 @Component({
   selector: 'app-register-patient',
   templateUrl: './register-patient.component.html',
@@ -7,9 +14,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPatientComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) { 
+    this._initForm();
+  }
+
+  //formGroup =  {
+      // personInfo = { formGroup},
+      // account = { formGroup }
+      // cuidador = {  formGroup}
+  // }
+
+  // Initialize the form.
+  private _initForm(){
+    this.form = this.fb.group(
+      {
+        nombre: ['', this._nameValidators],
+        appaterno: ['', this._nameValidators],
+        apmaterno: ['', this._nameValidators],
+        fnacimiento: [Validators.required],
+        genero: [Validators.required],
+        
+        usuario: ['', [Validators.required, Validators.minLength(6)]],
+        contrasenia: ['', [Validators.required, Validators.minLength(6)]],
+        contraseniaConfirm: ['', [Validators.required, Validators.minLength(6)]],
+        
+        nombreCuidador: ['', Validators.required, ], 
+        parentesco: [Validators.required],
+        generoCuidador: [Validators.required],
+        telefono: ['', Validators.required]
+
+      }
+    )
+  }
 
   ngOnInit(): void {
   }
 
+  // Validators for name control
+  private get _nameValidators(): Validators []{
+    return [
+      Validators.required, 
+      Validators.pattern(REGEX_NAME)
+    ]
+  }
+
+  getControl(controlName: string){
+    return this.form.get(controlName);
+  }
+
+  // Verify if a control is invalid
+  // Return: true if invalid, false otherwise.
+  isInvalid(controlName: string): boolean{
+    const control = this.form.get(controlName);
+    return control.invalid && control.touched;
+  }
+  
+  onSubmit(){
+    // console.log('Nombre: ',this.form.controls['nombre'].value);
+     
+  }
 }
